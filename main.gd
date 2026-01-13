a4d7f365df77787fef7e6591295edff9b7ab5c70
musta5839-sketch <musta5839@email.com>
2026-01-14 00:08:27 +0200

    Final Version: Add Background and Polish UI

--- main.gd
+++ main.gd
@@ -3,11 +3,8 @@ extends Node2D
 var score = 0
 
 func _ready():
-    update_score_label()
+    $ScoreLabel.text = "Score: 0"
 
 func _on_button_pressed():
     score += 1
-    update_score_label()
-
-func update_score_label():
     $ScoreLabel.text = "Score: " + str(score)
